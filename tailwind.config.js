400b863119cf9c17d46ba14450544d7eeef6d2a0
Waseem Mehany <seemsmacbookair@seems-macbook.lan>
2022-08-22 01:40:00 -0500

    Updated home page and nav bar with round 1 edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
         'primary-blue':'#30475E',
         'secondary-blue': '#4C6C98',
         'secondary-bg': 'rgba(76, 108, 152, 0.05)',
-        'text-black': '#222222',
-        'text-white': '#FFFFFF',
-        'slate-gray': '#6C7987',
+        'sc-black': '#222222',
+        'sc-white': '#FFFFFF',
+        'sc-slate-gray': '#6C7987',
       },
       fontSize: {
         '3.5xl': ['2rem', '2.5rem'],
