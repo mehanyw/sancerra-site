748a0548e087831025818df9806b4bfa78151206
Waseem Mehany <seemsmacbookair@seems-macbook.lan>
2022-09-01 16:04:00 -0500

    Updated text, footer, carousel, and a few minor details

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
 function carouselTimeOut(item)
 {
     setTimeout(() => {
-        console.log(item);
         item = item === 3 ? 1 : item + 1;
 
         // Again
         carouselTimeOut(item);
 
-        // Every 3 sec
-    }, 3000);
+        // Every 6 sec
+    }, 10000);
 }
